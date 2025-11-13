cdb871b75cdc64978e0a3ccd110566bb7b6c4502
Granola5791 <yuvalby1969@gmail.com>
2025-11-13 19:06:46 +0200

    created styled buttons and styled text.

---
+++ front-end/src/styled-components/StyledButtons.ts
@@ -0,0 +1,27 @@
+import styled from "@emotion/styled";
+import { Button } from "@mui/material";
+import { Colors } from "../constants/general-contants.ts";
+
+
+export const LongButton = styled(Button)({
+    width: "300px",
+    height: "50px",
+    fontSize: "20px",
+    fontWeight: "bold",
+    borderRadius: "20px",
+    border: `2px solid lightgray`,
+    color: "gray",
+    "&:active": {
+        // borderColor: "black",
+        // color: "black",
+    },
+});
+
+export const LongButtonFilled = styled(LongButton)({
+    color: "white",
+    backgroundColor: Colors.primary,
+    "&:hover": {
+        backgroundColor: Colors.primaryHover,
+    },
+});
+
---
+++ front-end/src/styled-components/StyledText.ts
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+import { Colors } from "../constants/general-contants.ts";
+
+export const StyledTitle = styled.h1 ({
+    fontSize: '3rem',
+    fontWeight: 'bold',
+    color: Colors.primary,
+    textAlign: 'center',
+});
