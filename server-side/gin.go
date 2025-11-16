1d4c00e5ae859212d0563fe335821c0f744b59ff
Granola5791 <yuvalby1969@gmail.com>
2025-11-16 17:41:33 +0200

    added api to server with an api for signup.

---
+++ server-side/gin.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func InitRouter() {
+	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{GetStringFromConfig("server.frontend_addr")},
+		AllowMethods:     []string{"POST", "GET", "OPTIONS", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Content-Type"},
+		AllowCredentials: true,
+	}))
+
+	router.POST(GetStringFromConfig("server.api.signup_path"), HandleSignup)
+
+	router.Run(GetStringFromConfig("server.listen_addr"))
+}
--- server-side/signup.go
+++ server-side/signup.go
@@ -1,6 +1,24 @@
 package main
 
-func SignUpUser(username string, password string) error {
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type UserSignupInput struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+func IsValidPassword(password string) bool {
+	return len(password) >= GetIntFromConfig("signup.min_password_length")
+}
+
+func IsValidUsername(username string) bool {
+	return len(username) >= GetIntFromConfig("signup.min_username_length")
+}
+
+func SignupUser(username string, password string) error {
 	hashedPassword, salt, err := GenerateNewHashedPassword(password)
 	if err != nil {
 		return err
@@ -11,4 +29,37 @@ func SignUpUser(username string, password string) error {
 	}
 
 	return nil
+}
+
+func HandleSignup(c *gin.Context) {
+	var input UserSignupInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": GetStringFromConfig("errors.invalid_input")})
+		return
+	}
+
+	if !IsValidUsername(input.Username) || !IsValidPassword(input.Password) {
+		c.JSON(http.StatusBadRequest, gin.H{"error": GetStringFromConfig("errors.invalid_input")})
+		return
+	}
+
+	userExists, err := UserExistsInDB(input.Username)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": GetStringFromConfig("errors.internal_server_error")})
+		return
+	}
+	if userExists {
+		c.JSON(http.StatusConflict, gin.H{"error": GetStringFromConfig("errors.user_exists")})
+		return
+	}
+
+	err = SignupUser(input.Username, input.Password)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": GetStringFromConfig("errors.internal_server_error")})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"success": GetStringFromConfig("success.user_created")})
 }
