e9008e9aba8961404b192edcb0500c6245266737
Granola5791 <yuvalby1969@gmail.com>
2026-03-01 02:38:09 +0200

    added https to the frontend.

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs';
+import path from 'path';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), ''); 
+
+    return {
+        plugins: [react()],
+        server: {
+            https: {
+                key: fs.readFileSync(path.resolve(__dirname, env.TLS_KEY_PATH)),
+                cert: fs.readFileSync(path.resolve(__dirname, env.TLS_CERT_PATH)),
+            },
+            host: env.SERVER_NAME,
+            port: 5173,
+        }
+    }
 })
