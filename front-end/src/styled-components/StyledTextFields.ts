3b51609287d713f1fb885479aab518e444c555ad
Granola5791 <yuvalby1969@gmail.com>
2026-03-29 16:06:13 +0300

    fixed label-outline alignment.

--- front-end/src/styled-components/StyledTextFields.ts
+++ front-end/src/styled-components/StyledTextFields.ts
@@ -11,6 +11,13 @@ export const StyledTextField = styled(TextField)({
         // Ensure it shrinks toward the right side
         transformOrigin: 'top right',
     },
+    '& .MuiInputLabel-shrink': {
+        transform: 'translate(0, -8px) scale(0.75) !important', // keeps label vertically centered in the gap
+        right: '15px', // aligns label with notched outline gap
+    },
+    '& .MuiOutlinedInput-notchedOutline': {
+        textAlign: 'right', // moves the legend/gap to the right side
+    },
     '& .MuiFilledInput-root': {
         // Fixes gray gaps in the background of the text field
         backgroundColor: '#e3f2fd',
