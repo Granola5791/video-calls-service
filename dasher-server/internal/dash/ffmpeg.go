e37823aac174df062fabc06b2856ab54f7e08262
Granola5791 <yuvalby1969@gmail.com>
2026-04-28 02:45:46 +0300

    ffmpeg now generates multiple resolutoins.

--- dasher-server/internal/dash/ffmpeg.go
+++ dasher-server/internal/dash/ffmpeg.go
@@ -16,19 +16,15 @@ func InitMpegDash(meetingID string, userID uint) (*exec.Cmd, io.WriteCloser, err
 		"-f", "webm",
 		"-i", "pipe:0",
 
-		// "-filter_complex",
-		// "[0:v]split=3[v1][v2][v3];"+
-		// 	"[v1]scale=854:480[v1out];"+
-		// 	"[v2]scale=640:360[v2out];"+
-		// 	"[v3]scale=426:240[v3out]",
+		"-filter_complex",
+		"[0:v]split=3[v1][v2][v3];"+
+			"[v1]scale=854:480[v1out];"+
+			"[v2]scale=640:360[v2out];"+
+			"[v3]scale=426:240[v3out]",
 
-		// "-map", "[v1out]",
-		// "-map", "[v2out]",
-		// "-map", "[v3out]",
-
-		"-s", "854x480",
-
-		"-map", "0:v",
+		"-map", "[v1out]",
+		"-map", "[v2out]",
+		"-map", "[v3out]",
 
 		"-map", "0:a",
 		"-c:a", "libopus",
@@ -46,27 +42,21 @@ func InitMpegDash(meetingID string, userID uint) (*exec.Cmd, io.WriteCloser, err
 
 		"-profile:v", "main",
 
-		// "-b:v:0", "1200k",
-		// "-b:v:1", "750k",
-		// "-b:v:2", "300k",
-
-		"-b:v", "600k",
+		"-b:v:0", "1200k",
+		"-b:v:1", "750k",
+		"-b:v:2", "300k",
 
 		"-f", "dash",
 		"-ldash", "1",
 		"-streaming", "1",
 		"-seg_duration", "0.25",
-		// "-min_seg_duration", "1000000",
-		// "-frag_duration", "0.1",
-		// "-frag_type", "duration",
 		"-use_template", "1",
 		"-use_timeline", "1",
 		"-utc_timing_url", "https://time.akamai.com/?iso",
 		"-window_size", "5",
 		"-extra_window_size", "5",
 		"-write_prft", "1",
-		// "-adaptation_sets", "id=0,streams=0,1,2 id=1,streams=3",
-		"-adaptation_sets", "id=0,streams=0 id=1,streams=1",
+		"-adaptation_sets", "id=0,streams=0,1,2 id=1,streams=3",
 		"-remove_at_exit", "0",
 		fmt.Sprintf("%s/%s/%d/stream.mpd", config.GetString("meeting.dir_path"), meetingID, userID),
 	)
